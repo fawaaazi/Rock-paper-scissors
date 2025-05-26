a933bca25021be2f50c113b0e865c5527d5a5655
fawaaazi <muhammedfawas90.com>
2025-05-26 23:11:28 +0530

    function playRound completed no GUI is used for game only alert is used fuction is completed using if else block and three options are made mapping of [1,2,3] and then made game

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-let humanChoice=getHumanChoice()
-let computerChoice=getComputerChoice()
 let computerScore=0
 let humanScore=0
 
 function getComputerChoice(){
-    let number=floor(Math.random()*3)
+    let number=Math.floor(Math.random()*3)
     if(number==0){
-        return "Rock"
+        return "ROCK"
     }
     else if(number==1){
-        return "Paper"
+        return "PAPER"
     }
     else{
-        return "Scissors"
+        return "SCISSOR"
     }
 }
 
@@ -28,7 +26,82 @@ function getHumanChoice(){
     }
 }
 
-function playRound(humanChoice,computerChoice){
-    
+function playRound(humanChoice=getHumanChoice(),computerChoice=getComputerChoice()){
+    const convertor={
+        ROCK : 1,
+        PAPER : 2,
+        SCISSOR : 3    
+    }
+
+    if(convertor[humanChoice]==1 || convertor[humanChoice]==3 && (convertor[computerChoice]==3 || convertor[computerChoice]==1)){
+        if(convertor[humanChoice]==1 && convertor[computerChoice] != 1){
+            humanScore+=1
+            alert("you win "+humanChoice+" beats "+computerChoice)
+            alert('computer score:'+computerScore)
+            alert("your score:"+humanScore)
+        }
+        else if(convertor[computerChoice]==convertor[humanChoice]){
+            alert("Draw")
+        }
+        else{
+            computerScore+=1
+            alert("you lose "+computerChoice+" beats "+humanChoice)
+            alert('computer score:'+computerScore)
+            alert("your score:"+humanScore)
+        }
+        
+    }
+    else if(convertor[humanChoice]==2 || convertor[humanChoice]==1 && (convertor[computerChoice]==2 || convertor[computerChoice]==1)){
+       
+        if(convertor[humanChoice]==2 && convertor[computerChoice] != 2){
+            humanScore+=1
+            alert("you win "+humanChoice+" beats "+computerChoice)
+            alert('computer score:'+computerScore)
+            alert("your score:"+humanScore)
+        }
+        else if(convertor[computerChoice]==convertor[humanChoice]){
+            alert("Draw")
+        }
+        else{
+            computerScore+=1
+            alert("you lose "+computerChoice+" beats "+humanChoice)
+            alert('computer score:'+computerScore)
+            alert("your score:"+humanScore)
+        }
+    }
+   else if(convertor[humanChoice]==2 || convertor[humanChoice]==3 && (convertor[computerChoice]==2 || convertor[computerChoice]==3)){
+       
+        if(convertor[humanChoice]==3 && convertor[computerChoice] != 3){
+            humanScore+=1
+            alert("you win "+humanChoice+" beats "+computerChoice)
+            alert('computer score:'+computerScore)
+            alert("your score:"+humanScore)
+        }
+        else if(convertor[computerChoice]==convertor[humanChoice]){
+            alert("Draw")
+        }
+        else{
+            computerScore+=1
+            alert("you lose"+computerChoice+" beats"+humanChoice)
+            alert('computer score:'+computerScore)
+            alert("your score:"+humanScore)
+        }
+    }
 
 }
+function startGame(){
+    while(humanScore < 5 && computerScore < 5){
+    playRound()
+    }
+
+    if(humanScore==5){
+     alert("Game over you win")
+        alert('computer score:'+computerScore+" your score :"+humanScore)
+    }
+    else{
+        alert("Game over you lose")
+        alert('computer score:'+computerScore+" your score :"+humanScore)
+    }
+
+}
+
